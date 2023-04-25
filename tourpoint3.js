0f9cf077804a828881d777384e1c0260f2851b71
meownoi1 <124670798+meownoi1@users.noreply.github.com>
2023-04-25 16:12:53 +0700

    Add files via upload

---
+++ tourpoint3.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หนองคาย",
+          "type": "พิเศษ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.74565580378419,
+            17.877908560198065
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หาดจอมมณี",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.7132740000016,
+            17.877669372631644
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศาลาแก้วกู่",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.7819930423293,
+            17.887380045601844
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดท่าเสด็จ",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.74831172883898,
+            17.88538821034018
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูห้วยอีสัน",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.1699595423272,
+            18.16135438870576
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกธารทอง",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.36554200000148,
+            18.02746740427895
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดหินหมากเป้ง",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.42917800000032,
+            17.98315981881845
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดผาตากเสื้อ",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.3051337116421,
+            18.037001403172866
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดผาตากเสื้อ",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.30241400000102,
+            17.96146820595621
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดภูนกกระบา",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.27916899999764,
+            17.983467204684132
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
